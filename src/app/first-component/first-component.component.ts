229f81748e5ef8533ccc6d3cb72c247d5d780c8b
Akash Kumar <akash.kumar651@gmail.com>
2019-07-10 23:44:15 +0530

    Modified new component

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class FirstComponentComponent implements OnInit {
 
   constructor() { }
-
+  firstcomponent = "Entered in new component created";
   ngOnInit() {
   }
 
